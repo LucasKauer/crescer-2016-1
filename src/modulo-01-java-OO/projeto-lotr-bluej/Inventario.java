62b25fd66c767bffb55eb6c1d073a0093a6b0459
Crescer <crescer@cwinet.local>
2016-04-08 16:26:06 -0300

    feat(Inventario): adicionar getDescricoesItens.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
@@ -14,4 +14,45 @@ public class Inventario {
     public ArrayList<Item> getItens() {
         return this.itens;
     }
+    
+    public String getDescricoesItens() {
+        String descricao = "";
+        
+        for (Item item : this.itens) {
+            descricao += item.getDescricao() + ",";
+        }
+        
+        // C#
+        // foreach (var item in this.itens)
+        
+        /*for (int i = 0; i < this.itens.size(); i++) {
+            descricao += this.itens.get(i).getDescricao() + ",";
+        }*/
+        /*
+        int i = 0;
+        while (i < this.itens.size()) {
+            descricao += this.itens.get(i).getDescricao() + ",";
+            i++;
+        }*/
+        /*
+        int i = 0;
+        do {
+            descricao += this.itens.get(i).getDescricao() + ",";
+            i++;
+        } while (i < this.itens.size());
+        */
+        
+        return !this.itens.isEmpty() ? descricao.substring(0, descricao.length() - 1) : descricao;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InventarioTest {
+    @Test
+    public void getDescricoesItensVazio() {
+        Inventario inventario = new Inventario();
+        String obtido = inventario.getDescricoesItens();
+        assertEquals("", obtido);
+    }
+
+    @Test
+    public void getDescricoesComUmItem() {
+        Inventario inventario = new Inventario();
+        inventario.adicionarItem(new Item(1, "Espada"));
+        String obtido = inventario.getDescricoesItens();
+        assertEquals("Espada", obtido);
+    }
+
+    @Test
+    public void getDescricoesComDoisItens() {
+        Inventario inventario = new Inventario();
+        inventario.adicionarItem(new Item(1, "Espada"));
+        inventario.adicionarItem(new Item(1, "Escudo"));
+        String obtido = inventario.getDescricoesItens();
+        assertEquals("Espada,Escudo", obtido);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
