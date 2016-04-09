2ac22eb39ef8989ea6a167b7a6c7872b52f0c719
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-04-09 11:27:12 -0300

    refactor(HobbitContador): centralizar lógica de cálculo do MMC em uma nested class.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -3,29 +3,37 @@ import java.util.ArrayList;
 public class HobbitContador {
     public int calcularDiferenca(ArrayList<ArrayList<Integer>> pares) {
         int somaProdutos = 0, somaMmc = 0;
-        
+
         for (ArrayList<Integer> par : pares) {
             int a = par.get(0), b = par.get(1);
             somaProdutos += a * b;
-            somaMmc += mmc(a, b);
+            somaMmc += new CalculadorMmc(a, b).calcular();
         }
 
         return somaProdutos - somaMmc;
     }
 
-    // https://en.wikipedia.org/wiki/Least_common_multiple
-    private int mmc(int a, int b) {
-        boolean temZero = a == 0 || b == 0;
-        return temZero ? 0 : a * b / mdc(a,b);
-    }
-
-    // https://en.wikipedia.org/wiki/Greatest_common_divisor
-    private int mdc(int a, int b) {
-        while (b != 0) {
-            int troca = b;
-            b = a % b;
-            a = troca;
+    private class CalculadorMmc {
+        private int a, b;
+        private CalculadorMmc(int a, int b) {
+            this.a = a;
+            this.b = b;
         }
-        return a;
+
+        // https://en.wikipedia.org/wiki/Least_common_multiple
+        private int calcular() {
+            boolean temZero = a == 0 || b == 0;
+            return temZero ? 0 : a * b / mdc(a,b);
+        }
+
+        // https://en.wikipedia.org/wiki/Greatest_common_divisor
+        private int mdc(int a, int b) {
+            while (b != 0) {
+                int troca = b;
+                b = a % b;
+                a = troca;
+            }
+            return a;
+        }
     }
 }
