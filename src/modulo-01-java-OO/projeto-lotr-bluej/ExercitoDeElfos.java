f79803dfdc526d42ea0c3530eedb67ec928edef8
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-04-13 23:39:48 -0300

    feat(ExercitoDeElfos): agrupar e pesquisar por status.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
@@ -2,20 +2,40 @@ import java.util.*;
 
 public class ExercitoDeElfos {
     private HashMap<String, Elfo> exercito = new HashMap<>();
-    
+    private HashMap<Status, ArrayList<Elfo>> porStatus = new HashMap<>();
+
     public void alistar(Elfo elfo) {
         boolean podeAlistar = elfo instanceof ElfoVerde || elfo instanceof ElfoNoturno;
-        
+
         if (podeAlistar) {
             exercito.put(elfo.getNome(), elfo);
         }
     }
-    
+
     public HashMap<String, Elfo> getExercito() {
         return this.exercito;
     }
-    
+
     public Elfo buscar(String nome) {
         return this.exercito.get(nome);
+    }
+
+    public void agruparPorStatus() {
+        for (Map.Entry<String, Elfo> chaveValor : this.exercito.entrySet()) {
+            Elfo elfo = chaveValor.getValue();
+            ArrayList<Elfo> statusNoAgrupamento = porStatus.get(elfo.getStatus());
+            boolean aindaNaoTenhoStatusNoAgrupamento = statusNoAgrupamento == null;
+
+            if (aindaNaoTenhoStatusNoAgrupamento) {
+                statusNoAgrupamento = new ArrayList<Elfo>(Arrays.asList(elfo));
+                porStatus.put(elfo.getStatus(), statusNoAgrupamento);
+            }
+
+            statusNoAgrupamento.add(elfo);
+        }
+    }
+
+    public ArrayList<Elfo> buscar(Status status) {
+        return this.porStatus.get(status);
     }
 }
--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfosTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfosTest.java
@@ -132,4 +132,77 @@ public class ExercitoDeElfosTest {
         exercito.alistar(elfo);
         assertNull(exercito.buscar("Night Legolas"));
     }
+
+    @Test
+    public void buscaElfosVivos() {
+        // Arrange
+        ElfoVerde elfoVivo1 = new ElfoVerde("Green 1");
+        ElfoNoturno elfoVivo2 = new ElfoNoturno("Aa");
+        ElfoVerde elfoVivo3 = new ElfoVerde("BB");
+        Elfo elfoMorto1 = criarElfoEMataLo("Dead Elf 1");
+        Elfo elfoMorto2 = criarElfoEMataLo("Dead Elf 2");
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistar(elfoMorto1);
+        exercito.alistar(elfoMorto2);
+        exercito.alistar(elfoVivo1);
+        exercito.alistar(elfoVivo2);
+        exercito.alistar(elfoVivo3);
+        // Act
+        exercito.agruparPorStatus();
+        ArrayList<Elfo> vivos = exercito.buscar(Status.VIVO);
+        // Assert
+        assertTrue(vivos.contains(elfoVivo1));
+        assertTrue(vivos.contains(elfoVivo2));
+        assertTrue(vivos.contains(elfoVivo3));
+    }
+
+    @Test
+    public void buscaElfosMortos() {
+        // Arrange
+        ElfoVerde elfoVivo1 = new ElfoVerde("Green 1");
+        ElfoNoturno elfoVivo2 = new ElfoNoturno("Aa");
+        ElfoVerde elfoVivo3 = new ElfoVerde("BB");
+        Elfo elfoMorto1 = criarElfoEMataLo("Dead Elf 1");
+        Elfo elfoMorto2 = criarElfoEMataLo("Dead Elf 2");
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistar(elfoMorto1);
+        exercito.alistar(elfoMorto2);
+        exercito.alistar(elfoVivo1);
+        exercito.alistar(elfoVivo2);
+        exercito.alistar(elfoVivo3);
+        // Act
+        exercito.agruparPorStatus();
+        ArrayList<Elfo> mortos = exercito.buscar(Status.MORTO);
+        // Assert
+        assertTrue(mortos.contains(elfoMorto1));
+        assertTrue(mortos.contains(elfoMorto2));
+    }
+
+    @Test
+    public void buscaElfosInconsciente() {
+        // Arrange
+        ElfoVerde elfoVivo1 = new ElfoVerde("Green 1");
+        ElfoNoturno elfoVivo2 = new ElfoNoturno("Aa");
+        ElfoVerde elfoVivo3 = new ElfoVerde("BB");
+        Elfo elfoMorto1 = criarElfoEMataLo("Dead Elf 1");
+        Elfo elfoMorto2 = criarElfoEMataLo("Dead Elf 2");
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistar(elfoMorto1);
+        exercito.alistar(elfoMorto2);
+        exercito.alistar(elfoVivo1);
+        exercito.alistar(elfoVivo2);
+        exercito.alistar(elfoVivo3);
+        exercito.agruparPorStatus();
+        // Act & Assert
+        assertNull(exercito.buscar(Status.INCONSCIENTE));
+    }
+
+    private Elfo criarElfoEMataLo(String nome) {
+        Elfo elfo = new ElfoNoturno(nome);
+        // Forçando o hara-kiri
+        for (int i = 0; i < 90; i++) {
+            elfo.atirarFlecha(new Dwarf("Gimli"));
+        }
+        return elfo;
+    }
 }
