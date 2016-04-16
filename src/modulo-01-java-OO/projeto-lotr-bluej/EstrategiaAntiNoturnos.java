79581df7c528c7fe38dc4d15be94315b04aacfd4
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-04-16 18:03:51 -0300

    refactor(EstrategiaAntiNoturnos): remover iteração desnecessária para buscar os elfos vivos.

--- src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaAntiNoturnos.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaAntiNoturnos.java
@@ -10,42 +10,29 @@ public class EstrategiaAntiNoturnos implements EstrategiaDeAtaque {
     public void atacar(ArrayList<Elfo> pelotao, ArrayList<Dwarf> dwarves) {
         this.ordemDoUltimoAtaque.clear();
 
-        // 1 - Filtrar VIVOS
-        // C#
-        // var vivos = elfos.Where(x => x.Status == Status.VIVO);
-        ArrayList<Elfo> vivos = vivos(pelotao);
-
-        // 2 - Variáveis de controle para quantidade de ataques.
-        int intencoesAtaque = vivos.size() * dwarves.size();
+        // 1 - Variáveis de controle para quantidade de ataques.
+        int intencoesAtaque = pelotao.size() * dwarves.size();
         int limiteAtaquesElfosNoturnos = (int)(intencoesAtaque * 0.3);
         int qtdAtaquesFeitosPorNoturnos = 0;
 
-        // 3 - Percorrendo lista de vivos para desferir os ataques seguindo as regras
-        for (Elfo elfoQueVaiAtacar : vivos) {
-            for (Dwarf dwarf : dwarves) {
-                boolean éElfoNoturno = elfoQueVaiAtacar instanceof ElfoNoturno;
+        // 2 - Percorrendo lista de vivos para desferir os ataques seguindo as regras
+        for (Elfo elfoQueVaiAtacar : pelotao) {
+            if (elfoQueVaiAtacar.getStatus() == Status.VIVO) {
+                for (Dwarf dwarf : dwarves) {
+                    boolean éElfoNoturno = elfoQueVaiAtacar instanceof ElfoNoturno;
 
-                if (éElfoNoturno) {
-                    boolean devePularParaOProximo = qtdAtaquesFeitosPorNoturnos >= limiteAtaquesElfosNoturnos;
-                    if (devePularParaOProximo) {
-                        continue;
+                    if (éElfoNoturno) {
+                        boolean devePularParaOProximo = qtdAtaquesFeitosPorNoturnos >= limiteAtaquesElfosNoturnos;
+                        if (devePularParaOProximo) {
+                            continue;
+                        }
+                        qtdAtaquesFeitosPorNoturnos++;
                     }
-                    qtdAtaquesFeitosPorNoturnos++;
+
+                    ordemDoUltimoAtaque.add(elfoQueVaiAtacar);
+                    elfoQueVaiAtacar.atirarFlecha(dwarf);
                 }
-
-                ordemDoUltimoAtaque.add(elfoQueVaiAtacar);
-                elfoQueVaiAtacar.atirarFlecha(dwarf);
             }
         }
-    }
-
-    private ArrayList<Elfo> vivos(ArrayList<Elfo> pelotao) {
-        ArrayList<Elfo> vivos = new ArrayList<>();
-        for (Elfo elfo : pelotao) {
-            if (elfo.getStatus() == Status.VIVO) {
-                vivos.add(elfo);
-            }
-        }
-        return vivos;
     }
 }
