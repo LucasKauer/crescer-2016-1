57d34212dab6f92ba9ecb67aec05650d591ed28c
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-04-16 17:14:43 -0300

    feat(EstrategiaPriorizandoElfosVerdes): adicionar estratégia que prioriza elfos verdes em relação aos noturnos.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaPriorizandoElfosVerdes.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class EstrategiaPriorizandoElfosVerdes implements EstrategiaDeAtaque {
+    private ArrayList<Elfo> ordemAtaque = new ArrayList<>();
+
+    public void atacar(ArrayList<Elfo> elfos, ArrayList<Dwarf> dwarves) {
+        //ordenaComBubbleSort(elfos);
+        ordenaComCollectionSort(elfos);
+
+        for (Elfo elfo : elfos) {
+            ordemAtaque.add(elfo);
+            for (Dwarf dwarf : dwarves) {
+                elfo.atirarFlecha(dwarf);
+            }
+        }
+    }
+
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return ordemAtaque;
+    }
+    
+    private void ordenaComCollectionSort(ArrayList<Elfo> elfos) {
+        Collections.sort(elfos, new Comparator<Elfo>() {
+                public int compare(Elfo elfoAtual, Elfo proximoElfo) {
+                    boolean mesmoTipo = elfoAtual.getClass() == proximoElfo.getClass();
+
+                    if (mesmoTipo) {
+                        return 0;
+                    }
+
+                    return elfoAtual instanceof ElfoVerde && proximoElfo instanceof ElfoNoturno ? -1 : 1;
+                }
+            });
+    }
+
+    private void ordenaComBubbleSort(ArrayList<Elfo> elfos) {
+
+        boolean houverTroca = true;
+        while (houverTroca) {
+            houverTroca = false;
+            for (int i = 0; i < elfos.size() - 1; i++) {
+
+                Elfo elfoAtual = elfos.get(i);
+                Elfo elfoProximo = elfos.get(i + 1);
+
+                // só precisa trocar se um elfo noturno vier antes de um elfo verde
+                boolean precisaTrocar = elfoAtual instanceof ElfoNoturno && elfoProximo instanceof ElfoVerde;
+
+                if (precisaTrocar) {
+                    elfos.set(i, elfoProximo);
+                    elfos.set(i + 1, elfoAtual);
+                    houverTroca = true;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaPriorizandoElfosVerdesTest.java
@@ -0,0 +1,70 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+
+public class EstrategiaPriorizandoElfosVerdesTest {
+    @Test
+    public void exercitoEmbaralhadoPriorizaAtaqueComElfosVerdes() throws NaoPodeAlistarException {
+        // Arrange
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.mudarEstrategia(new EstrategiaPriorizandoElfosVerdes());
+        Elfo en1 = new ElfoNoturno("Night 1");
+        Elfo en2 = new ElfoNoturno("Night 2");
+        Elfo ev1 = new ElfoVerde("Green 1");
+        Elfo en3 = new ElfoNoturno("Night 3");
+        Elfo ev2 = new ElfoVerde("Green 2");
+        exercito.alistar(en1);
+        exercito.alistar(en2);
+        exercito.alistar(ev1);
+        exercito.alistar(en3);
+        exercito.alistar(ev2);
+        ArrayList<Elfo> esperado = new ArrayList<>(Arrays.asList(ev2, ev1, en3, en2, en1));
+        // Act
+        exercito.atacar(new ArrayList<>(Arrays.asList(new Dwarf("D1"), new Dwarf("D2"), new Dwarf("D3"))));
+        // Assert
+        assertEquals(esperado, exercito.getOrdemDoUltimoAtaque());
+    }
+
+    @Test
+    public void exercitoSóDeVerdes() throws NaoPodeAlistarException {
+        // Arrange
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.mudarEstrategia(new EstrategiaPriorizandoElfosVerdes());
+        Elfo ev1 = new ElfoVerde("Green 1");
+        Elfo ev2 = new ElfoVerde("Green 2");
+        exercito.alistar(ev1);
+        exercito.alistar(ev2);
+        ArrayList<Elfo> esperado = new ArrayList<>(Arrays.asList(ev2, ev1));
+        // Act
+        exercito.atacar(new ArrayList<>(Arrays.asList(new Dwarf("D1"), new Dwarf("D2"), new Dwarf("D3"))));
+        // Assert
+        assertEquals(esperado, exercito.getOrdemDoUltimoAtaque());
+    }
+    
+    @Test
+    public void exercitoSóDeNoturnos() throws NaoPodeAlistarException {
+        // Arrange
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.mudarEstrategia(new EstrategiaPriorizandoElfosVerdes());
+        Elfo en1 = new ElfoNoturno("EN1");
+        Elfo en2 = new ElfoNoturno("EN2");
+        exercito.alistar(en1);
+        exercito.alistar(en2);
+        ArrayList<Elfo> esperado = new ArrayList<>(Arrays.asList(en2, en1));
+        // Act
+        exercito.atacar(new ArrayList<>(Arrays.asList(new Dwarf("D1"), new Dwarf("D2"), new Dwarf("D3"))));
+        // Assert
+        assertEquals(esperado, exercito.getOrdemDoUltimoAtaque());
+    }
+    
+    @Test
+    public void ataqueComExercitoVazio() throws NaoPodeAlistarException {
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.mudarEstrategia(new EstrategiaPriorizandoElfosVerdes());
+        exercito.atacar(new ArrayList<>(Arrays.asList(new Dwarf("D1"), new Dwarf("D2"))));
+        List<Elfo> ordemAtaque = exercito.getOrdemDoUltimoAtaque();
+        assertTrue(ordemAtaque.isEmpty());
+    }
+}
