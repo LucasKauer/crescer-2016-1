934e05b5b5744c9d7ddebf445adb6d30a432f7ca
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-04-16 17:43:09 -0300

    feat(EstrategiaIntercalada): adicionar estratégia que divide os ataques de forma intercalada entre os tipos de elfos.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaIntercalada.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class EstrategiaIntercalada implements EstrategiaDeAtaque {
+    private ArrayList<Elfo> ordemAtaque = new ArrayList<>();
+
+    public void atacar(ArrayList<Elfo> elfos, ArrayList<Dwarf> dwarves) {
+
+        // garante que não ataca se a lista de elfos é vazia OU o exército não é proporcional (50%-50%)
+        if (elfos.isEmpty() || !validarProporcoes(elfos)) {
+            return;
+        }
+
+        intercalar(elfos);
+
+        for (Elfo elfo : ordemAtaque) {
+            for (Dwarf dwarf : dwarves) {
+                elfo.atirarFlecha(dwarf);
+            }
+        }
+    }
+
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return ordemAtaque;
+    }
+
+    private boolean validarProporcoes(ArrayList<Elfo> elfos) {
+        int contadorElfosVerdes = 0, contadorElfosNoturnos = 0;
+
+        for (Elfo elfo : elfos) {
+            if (elfo instanceof ElfoVerde) {
+                contadorElfosVerdes++;
+            } else if (elfo instanceof ElfoNoturno) {
+                contadorElfosNoturnos++;
+            }
+        }
+
+        return contadorElfosVerdes == contadorElfosNoturnos;
+    }
+
+    private void intercalar(ArrayList<Elfo> elfos) {     
+
+        Elfo primeiroElfo = elfos.get(0);
+        ordemAtaque.add(primeiroElfo);
+        Class classeDoUltimoAdicionado = primeiroElfo.getClass();
+        elfos.remove(primeiroElfo);
+
+        while (elfos.size() > 0) {
+            for (int j = 0; j < elfos.size(); j++) {
+                Elfo elfoAtual = elfos.get(j);
+                boolean estáIntercalado = elfoAtual.getClass() != classeDoUltimoAdicionado;
+
+                if (estáIntercalado) {
+                    ordemAtaque.add(elfoAtual);
+                    classeDoUltimoAdicionado = elfoAtual.getClass();
+                    elfos.remove(elfoAtual);
+                }
+            }
+        }
+    }
+}
---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaIntercaladaTest.java
@@ -0,0 +1,96 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+
+public class EstrategiaIntercaladaTest {
+    @Test
+    public void exercitoIntercaladoComeçandoComElfoNoturno() throws NaoPodeAlistarException {
+        // Arrange
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.mudarEstrategia(new EstrategiaIntercalada());
+        Elfo night1 = new ElfoNoturno("Night 1");
+        Elfo night2 = new ElfoNoturno("Night 2");
+        Elfo green1 = new ElfoVerde("Green 1");
+        Elfo night3 = new ElfoNoturno("Night 3");
+        Elfo green2 = new ElfoVerde("Green 2");
+        Elfo green3 = new ElfoVerde("Green 3");
+        exercito.alistar(night1);
+        exercito.alistar(night2);
+        exercito.alistar(green1);
+        exercito.alistar(night3);
+        exercito.alistar(green2);
+        exercito.alistar(green3);
+        List<Elfo> esperado = new ArrayList<>(Arrays.asList(night3, green2, night1, green1, night2, green3));
+        // Act
+        exercito.atacar(new ArrayList<>(Arrays.asList(new Dwarf("D1"), new Dwarf("D2"), new Dwarf("D3"))));
+        // Assert
+        ArrayList<Elfo> resultado = exercito.getOrdemDoUltimoAtaque();
+        assertEquals(esperado, resultado);
+    }
+
+    @Test
+    public void exercitoIntercaladoComeçandoComElfoVerde() throws NaoPodeAlistarException {
+        // Arrange
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.mudarEstrategia(new EstrategiaIntercalada());
+        Elfo night1 = new ElfoNoturno("EN1");
+        Elfo night2 = new ElfoNoturno("EN2");
+        Elfo green1 = new ElfoVerde("EV1");
+        Elfo green2 = new ElfoVerde("EV2");
+        exercito.alistar(night1);
+        exercito.alistar(night2);
+        exercito.alistar(green1);
+        exercito.alistar(green2);
+        List<Elfo> esperado = new ArrayList<>(Arrays.asList(green2, night2, green1, night1));
+        // Act
+        exercito.atacar(new ArrayList<>(Arrays.asList(new Dwarf("D1"), new Dwarf("D2"), new Dwarf("D3"))));
+        // Assert
+        ArrayList<Elfo> resultado = exercito.getOrdemDoUltimoAtaque();
+        assertEquals(esperado, resultado);
+    }
+
+    @Test
+    public void exercitoDesproporcionalNãoAtaca() throws NaoPodeAlistarException {
+        // Arrange
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.mudarEstrategia(new EstrategiaIntercalada());
+        Elfo night1 = new ElfoNoturno("Noturno 1");
+        Elfo night2 = new ElfoNoturno("Elfo Noturno 2");
+        Elfo green1 = new ElfoVerde("Elfo Verde 1");
+        exercito.alistar(green1);
+        exercito.alistar(night1);
+        exercito.alistar(night2);
+        // Act
+        exercito.atacar(new ArrayList<>(Arrays.asList(new Dwarf("D1"), new Dwarf("D2"), new Dwarf("D3"))));
+        // Assert
+        ArrayList<Elfo> resultado = exercito.getOrdemDoUltimoAtaque();
+        assertTrue(resultado.isEmpty());
+    }
+    
+    @Test
+    public void exercitoSoDeUmTipoNãoAtaca() throws NaoPodeAlistarException {
+        // Arrange
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.mudarEstrategia(new EstrategiaIntercalada());
+        Elfo night1 = new ElfoNoturno("Noturno 1");
+        Elfo night2 = new ElfoNoturno("Elfo Noturno 2");
+        exercito.alistar(night1);
+        exercito.alistar(night2);
+        // Act
+        exercito.atacar(new ArrayList<>(Arrays.asList(new Dwarf("D1"), new Dwarf("D2"), new Dwarf("D3"))));
+        // Assert
+        ArrayList<Elfo> resultado = exercito.getOrdemDoUltimoAtaque();
+        assertTrue(resultado.isEmpty());
+    }
+    
+    @Test
+    public void ataqueComExercitoVazio() throws NaoPodeAlistarException {
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.mudarEstrategia(new EstrategiaIntercalada());
+        exercito.atacar(new ArrayList<>(Arrays.asList(new Dwarf("D1"), new Dwarf("D2"))));
+        List<Elfo> ordemAtaque = exercito.getOrdemDoUltimoAtaque();
+        assertTrue(ordemAtaque.isEmpty());
+    }
+}
