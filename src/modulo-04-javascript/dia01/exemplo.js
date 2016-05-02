d494b9f11f8f84ba66caefcd3268d54eb9216e0f
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-05-02 20:13:39 -0300

    feat(function): adicionar exemplo de objeto com função.

--- src/modulo-04-javascript/dia01/exemplo.js
+++ src/modulo-04-javascript/dia01/exemplo.js
@@ -2,3 +2,22 @@
 
 var hello = 'Hello World';
 console.log(hello);
+
+var goku = {
+  nome: 'Son Goku',
+  classe: 'Saiyajin',
+  ki: 999500,
+  kamehameha: function() {
+    var vd = document.getElementById('videoKame');
+    var partes = [ 'Ka', 'Me', 'Ha', 'Me', 'Haaa!!!' ];
+    var indice = 0;
+    vd.style.display = 'block';
+    vd.play();
+    var id = setInterval(function() {  
+      if (indice === partes.length - 1) {
+        clearInterval(id);
+      }
+      document.body.appendChild(document.createTextNode(partes[indice++]));
+    }, 1400);
+  }
+};
