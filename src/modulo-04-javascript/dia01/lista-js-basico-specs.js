ab1cb87eb6bdf8b9508f5f0afde79a37ec23fd5b
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-05-02 21:42:48 -0300

    feat(maiorTexto): adicionar cálculo do maior texto do array.

--- src/modulo-04-javascript/dia01/lista-js-basico-specs.js
+++ src/modulo-04-javascript/dia01/lista-js-basico-specs.js
@@ -14,3 +14,21 @@ describe('Ex 1. Daisy Game!', function() {
     expect(daisyGame()).toBe('Love me');
   });
 });
+
+describe('Ex 2. maiorTexto', function() {
+  it('quando informa array vazio retorna vazio', function() {
+    expect(maiorTexto([])).toBe('');
+  });
+  it('quando informa array com apenas um elemento retorna o mesmo', function() {
+    expect(maiorTexto([ 'único' ])).toBe('único');
+  });
+  it('quando informa array com duas strings de mesmo tamanho retorna o primeiro', function() {
+    expect(maiorTexto([ 'm3sm0', 'mesmo' ])).toBe('m3sm0');
+  });
+  it('quando informa array com duas strings diferentes', function() {
+    expect(maiorTexto([ 'm3sm0', 'maior!!' ])).toBe('maior!!');
+  });
+  it('quando informa array com várias strings diferentes', function() {
+    expect(maiorTexto([ 'm3sm0', 'abacate', 'goku', 'yajirobe', 'freeza', 'kuririn' ])).toBe('yajirobe');
+  });
+});
--- src/modulo-04-javascript/dia01/lista-js-basico.js
+++ src/modulo-04-javascript/dia01/lista-js-basico.js
@@ -1,5 +1,15 @@
 'use strict';
 
+// Ex 1
 function daisyGame(numero) {
   return numero % 2 === 0 ? 'Love me not' : 'Love me';
 }
+
+// Ex 2
+function maiorTexto(arr) {
+  var maior = '';
+  arr.forEach(function(e) {
+    if (e.length > maior.length) maior = e;
+  });
+  return maior;
+};
