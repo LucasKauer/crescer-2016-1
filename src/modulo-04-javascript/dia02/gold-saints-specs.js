18bf27bd4ed6a666002823d50a0389005e4ba2b0
bernardobrezende@gmail.com <bernardobrezende@gmail.com>
2016-05-03 16:21:57 -0300

    feat(gold-saints): adicionar estrutura inicial para gold-saints.

---
+++ src/modulo-04-javascript/dia02/gold-saints-specs.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Ex 1. Doadores de sangue', function() {
+  it('quando informa 4 retorna \'Love me not\'', function() {
+    expect(obterDoadores()).toEqual([]);
+  });
+});
