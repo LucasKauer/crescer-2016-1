c504c1d515d98f9dd314b26db9e77adc43106fd5
Ben-hur <benhur.ott@gmail.com>
2016-05-16 16:45:14 -0300

    feat(ADO): adicionando correções para usuário repositório

---
+++ src/modulo-05-dotnet/Aula 7 - ADO/Exemplo ADO - WEB/Repositorio/SqlDataReaderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public static class SqlDataReaderExtensions
+    {
+        public static int ReadInt(this SqlDataReader leitor, string nomeColuna)
+        {
+            return Convert.ToInt32(leitor[nomeColuna]);
+        }
+    }
+}
---
+++ src/modulo-05-dotnet/Aula 7 - ADO/Exemplo ADO - WEB/Repositorio/UsuarioRepositorioADO.cs
@@ -0,0 +1,41 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class UsuarioRepositorioADO : RepositorioBase, IUsuarioRepositorio
+    {
+        public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+        {
+            using (var conexao = new SqlConnection(ConnectionString))
+            {
+                string sql = "SELECT TOP 1 * FROM Usuarios WHERE email = @p_email AND senha = @p_senha";
+
+                var comando = new SqlCommand(sql, conexao);
+                comando.Parameters.Add(new SqlParameter("p_email", email));
+                comando.Parameters.Add(new SqlParameter("p_senha", senha));
+
+                conexao.Open();
+
+                SqlDataReader leitor = comando.ExecuteReader();
+
+                Usuario usuarioEncontrado = null;
+
+                if(leitor.Read())
+                {
+                    usuarioEncontrado = new Usuario();
+                    usuarioEncontrado.Id = leitor.ReadInt("id");
+                    usuarioEncontrado.Email = leitor["email"].ToString();
+                    usuarioEncontrado.Nome = leitor["nome"].ToString();
+                }
+
+                return usuarioEncontrado;
+            }
+        }
+    }
+}
