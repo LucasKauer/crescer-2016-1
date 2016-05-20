57b80f7e49755d65c52537968d4b653bcbab584c
bernardobrezende@gmail.com <bernardobrezende@gmail.com>
2016-05-20 14:10:51 -0300

    fix(Busca Artista): adicionar tratamento para erro 400 na busca por artista.

--- src/modulo-06-ajax/dia02/correcao-tema/Douchebag/scripts/TelaConsulta.js
+++ src/modulo-06-ajax/dia02/correcao-tema/Douchebag/scripts/TelaConsulta.js
@@ -23,39 +23,46 @@ TelaConsulta.prototype.render = function() {
   this.form.on('submit', function(e) {
     self.progress.toggleClass('escondido');
     var jaAdicionados = [], artistaBusca = self.textoBusca.val();
-    
-    self.spotify.pesquisarArtista(artistaBusca).done(function(response) {
-      if (response.artists.items.length == 0) {
-        alert('Sem resultados encontrados');
-        self.progress.toggleClass('escondido');
-        return e.preventDefault();
-      }
-      var idParaPesquisar = self.getIdParaBusca(response.artists.items[0].id);
-      self.spotify.obterDetalhesArtista(idParaPesquisar).done(function(response) {
-        // zerando lista de albuns antes de renderizá-la
-        self.listaAlbuns.empty();
-        response.items.forEach(function(i) {
-          // lembram de prototype? poderíamos fazer um contains.
-          // !jaAdicionados.contains(i.name)
-          if (jaAdicionados.indexOf(i.name.toLowerCase()) === -1) {
-            self.listaAlbuns.append(
-              $('<li>').append(
-                $('<img>').attr('src', i.images[1].url)
-              )
-            );
-            console.log(i.name);
-            jaAdicionados.push(i.name.toLowerCase());
-          }
+
+    self.spotify.pesquisarArtista(artistaBusca)
+      .done(function(response) {
+        if (response.artists.items.length == 0) {
+          alert('Sem resultados encontrados');
+          self.progress.toggleClass('escondido');
+          return e.preventDefault();
+        }
+        var idParaPesquisar = self.getIdParaBusca(response.artists.items[0].id);
+        self.spotify.obterDetalhesArtista(idParaPesquisar).done(function(response) {
+          // zerando lista de albuns antes de renderizá-la
+          self.listaAlbuns.empty();
+          response.items.forEach(function(i) {
+            // lembram de prototype? poderíamos fazer um contains.
+            // !jaAdicionados.contains(i.name)
+            if (jaAdicionados.indexOf(i.name.toLowerCase()) === -1) {
+              self.listaAlbuns.append(
+                $('<li>').append(
+                  $('<img>').attr('src', i.images[1].url)
+                )
+              );
+              console.log(i.name);
+              jaAdicionados.push(i.name.toLowerCase());
+            }
+          });
+          self.progress.toggleClass('escondido');
+        })
+        .fail(function(err) {
+          self.listaAlbuns.empty();
+          var msg = String.format('Caro usuário, devido a um erro {0}, não foi possível pesquisar por {1}', err.status, artistaBusca);
+          alert(msg);
+          self.progress.toggleClass('escondido');
         });
-        self.progress.toggleClass('escondido');
-      })
-      .fail(function(err) {
-        self.listaAlbuns.empty();
-        var msg = String.format('Caro usuário, devido a um erro {0}, não foi possível pesquisar por {1}', err.status, artistaBusca);
-        alert(msg);
-        self.progress.toggleClass('escondido');
-      });
     })
+    .fail(function(err) {
+      if (err.status === 400) {
+        alert('Caro usuário, revise seus parâmetros de busca!');
+      };
+      self.progress.toggleClass('escondido');
+    });
 
     // evitando tratamento padrão do Browser
     return e.preventDefault();
