06e4178db18f7ef646ef69b88bf7ceaadafb7c6a
bernardobrezende@gmail.com <bernardobrezende@gmail.com>
2016-05-23 00:10:12 -0300

    fix(PesoLb, AlturaCm): adicionar modelbinder customizado para fazer parse de double nas models.

--- src/modulo-06-ajax/dia02/CdZ/src/CdZ.MVC/Global.asax.cs
+++ src/modulo-06-ajax/dia02/CdZ/src/CdZ.MVC/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using CdZ.MVC.CustomModelBinders;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -13,6 +10,8 @@ namespace CdZ.MVC
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(double), new DoubleModelBinder());
+            ModelBinders.Binders.Add(typeof(double?), new DoubleModelBinder());
         }
     }
 }
